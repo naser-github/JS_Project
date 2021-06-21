e63c222d166c35fae6719ff93109386dba16636a
tayson13 <ab.naser01@gmail.com>
2021-06-21 19:39:10 +0600

    21/6 7:38pm

--- data_structure/ex_5.js
+++ data_structure/ex_5.js
@@ -2,18 +2,32 @@
 
 //1
 
-const camelCase = function(variable){
-    let splitVar = variable.split('_');
-    let camelVar = [];
-    for(const splits of splitVar)
-        camelVar.push(splits.replace(splits[0], splits[0].toUpperCase() ) );
-    
-    camelVar = camelVar.join('').replace(camelVar[0],camelVar[0].toLowerCase() );
-    console.log(camelVar);
+const camelCase = function(variables){
+    for (const variable of variables){
+        let splitVar = variable.split('_');
+        let camelVar = [];
+        for(const splits of splitVar)
+            camelVar.push(splits.replace(splits[0], splits[0].toUpperCase() ) );
+        
+        camelVar = camelVar.join('').replace(camelVar[0],camelVar[0].toLowerCase() );
+        console.log(camelVar);
+    }
 }
 
-camelCase("joe_root_hold");
-
 //2
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').textContent= 'Click';
+
+document.querySelector('button').addEventListener('click',function(){
+    let val = [];
+    const value = document.querySelector('textarea').value;
+    val = value.split('\n');
+    //console.log(val);
+    camelCase(val);
+});
+
+
 
 //3
